e8cdf7515b976f85de87010331a48f875ac03d09
Mostafa Gaber Ahmed Hussien <119273107+MostafaGaber135@users.noreply.github.com>
2023-11-30 15:05:59 +0200

    add add note state

--- lib/cubits/features/add_note_cubit/presentation/cubit/add_note_state.dart
+++ lib/cubits/features/add_note_cubit/presentation/cubit/add_note_state.dart
@@ -5,3 +5,11 @@ part of 'add_note_cubit.dart';
 abstract class AddNotesState {}
 
 class AddNotesInitial extends AddNotesState{}
+class AddNotesLoading extends AddNotesState{}
+class AddNotesSuccess extends AddNotesState{}
+class AddNotesFailure extends AddNotesState{
+  final String errMessage;
+
+  AddNotesFailure({required this.errMessage});
+
+}
