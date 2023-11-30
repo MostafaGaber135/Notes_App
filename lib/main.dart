181a5fdab6dd5a4487556f0faa14e63a9ba4a33a
Mostafa Gaber Ahmed Hussien <119273107+MostafaGaber135@users.noreply.github.com>
2023-11-30 12:48:17 +0200

    take input and validate data

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:notes_app/constants.dart';
 import 'package:notes_app/views/notes_view.dart';
 
 void main()  async{
+
   await Hive.initFlutter();
   await Hive.openBox(kNotesBox);
   runApp(const NotesApp());
