8aac2031c538df20200cfd5280b3fd86dbd78923
Mostafa Gaber Ahmed Hussien <119273107+MostafaGaber135@users.noreply.github.com>
2023-12-07 03:27:54 +0200

    display notes part one

---
+++ lib/widgets/custom_notes_list_view.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/features/add_note_cubit/features/notes/presentation/notes_cubit/notes_cubit.dart';
+import 'package:notes_app/modals/note_model.dart';
+import 'package:notes_app/widgets/custom_note_item.dart';
+
+class NotesListView extends StatelessWidget {
+  const NotesListView({Key? key}) : super(key: key);
+
+@override
+Widget build(BuildContext context){
+  return BlocBuilder<NotesCubit,NotesState>(
+    builder: (context, state) {  
+      List<NoteModel> notes = BlocProvider.of<NotesCubit>(context).notes ?? [];
+      return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      child: ListView.builder(
+        itemCount: notes.length,
+        padding: EdgeInsets.zero,
+        itemBuilder: (context, index){
+          return const Padding(
+            padding: EdgeInsets.symmetric(vertical: 4),
+            child: CustomNoteItem(),
+          );
+        }),
+      );
+    },
+  );
+}
+}
